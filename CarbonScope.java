e0e54e9068f5b4c60565d40e4af11908d394e11f
jupiterbouncer <demisubair11@gmail.com>
2025-12-01 23:09:26 +0000

    More work on the GUI, testing drop down menu

--- CarbonScope.java
+++ CarbonScope.java
@@ -10,23 +10,48 @@ public class CarbonScope extends JFrame{
     private Home home;
     private double kgOfWaste;
 
-    private JTextField userName;
+    private JTextField userName, userLocation;
+
+    // For the Car form
+    private JTextField mileage, fuelConsumption;
+
     private JTextArea outputArea;
 
-    private JButton calculateButton;
+    private JButton homeButton, calculatorButton, summaryButton, tipsButton;
 
-    public CarbonScope () {
-        setTitle("CARBONSCOPE");
+    public CarbonScope() {
+
+        setTitle("CARBONSCOPE: YOUR CARBON FOOTPRINT ASSISTANT");
         setSize(1000,800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
 
-        add(new JLabel("User: "));
+        JTabbedPane carbonTab = new JTabbedPane();
+
+        add(new JLabel("Name: ")); 
         userName = new JTextField(15);
         add(userName);
 
-        calculateButton = new JButton("CALCULATE");
-        add(calculateButton);
+        add(new JLabel("Location: "));
+        userLocation = new JTextField(15);
+        add(userLocation);
+
+        calculatorButton = new JButton("CALCULATE");
+        add(calculatorButton);
+
+        // JPanel for the Vehicle class
+        JPanel vehiclePanel = new JPanel();
+        vehiclePanel.setBackground(Color.LIGHT_GRAY);
+        vehiclePanel.setLayout(new BorderLayout());
+        vehiclePanel.add(new JLabel("Select Vehicle Type"), BorderLayout.CENTER);
+
+        String [] vehicleType = {"Car", "Bus", "Aeroplane"};
+        JComboBox<String> vehicleComboBox = new JComboBox<>(vehicleType);
+
+        add(vehiclePanel);
+        add(vehicleComboBox);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
