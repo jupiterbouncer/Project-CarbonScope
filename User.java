39c1f87b63d944cf525e3c965a33eac5d7ac2319
deron777 <deron.asante-twum@ashesi.edu.gh>
2025-12-02 10:26:30 +0000

    Updated the User class to include name and location fields

--- User.java
+++ User.java
@@ -1,10 +1,14 @@
 public class User {  
+    private String name; 
+    private String location;
     private Vehicle vehicle; 
     private Home home; 
     private Diet diet; 
     private double kgofWaste;  
 
-    public User(Vehicle vehicle, Home home, Diet diet, double kgofWaste) { 
+    public User(String name, String location, Vehicle vehicle, Home home, Diet diet, double kgofWaste) { 
+        this.name = name;
+        thsi.location = location; 
         this.vehicle = vehicle;
         this.home = home;
         this.diet = diet; 
@@ -17,9 +21,15 @@ public class User {
     }; 
 
     public void addActivity(CarbonFootprint a){ 
-        this.totalUserFootprint() += a.totalCarbonFootprint();
+        //MUST IMPLEMENT THIS 
     }; 
 
+    public String getName() { 
+        return this.name; 
+    } 
+    public String getLocation() { 
+        return this.location;
+    } 
     public Vehicle getVehicle(){ 
         return this.vehicle;
     } 
@@ -33,6 +43,13 @@ public class User {
     } 
     public Home getHome(){
         return this.home;
+    } 
+    
+    public void setName(String name){ 
+        this.name = name;
+    } 
+    public void setLocation(String location){ 
+        this.location = location;
     } 
 
     public void setDiet(Diet diet){
