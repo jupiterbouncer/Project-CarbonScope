797593a645c8c57ec204ba502bd9a0c7d91a2d4f
jupiterbouncer <demisubair11@gmail.com>
2025-12-09 17:07:13 +0000

    Update Aeroplane.java

--- Aeroplane.java
+++ Aeroplane.java
@@ -8,14 +8,21 @@ public class Aeroplane extends Vehicle{
         setFlightsPerYear(flightsPerYear);
     }
 
-    // Getter method
+    // Getter method for the Aeroplane class
     public int getFlightsPerYear(){
         return this.flightsPerYear;
     }
 
-    // Setter method
+    // Setter method for the Aeroplane class
     public void setFlightsPerYear(int flightsPerYear){
         if (flightsPerYear < 0) throw new IllegalArgumentException("Flights per year cannot be negative");
         else this.flightsPerYear = flightsPerYear;
     }
+
+    // Calculating the carbon footprint
+    @Override
+    public double calculateFootprint(){
+        return flightsPerYear * super.calculateFootprint();
+    }
+
 }
